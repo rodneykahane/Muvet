df914401af028065d818199576565b76901d9ed7
rodney <rodney@rodneykahan.es>
2019-07-21 14:21:29 -0400

    adding to navigation

---
+++ app/src/main/java/com/aaclogic/muvet/SignInRegister.java
@@ -0,0 +1,14 @@
+package com.aaclogic.muvet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.new_user_user);
+    }
+}
--- app/src/main/java/com/aaclogic/muvet/UserType.java
+++ app/src/main/java/com/aaclogic/muvet/UserType.java
@@ -1,7 +1,9 @@
 package com.aaclogic.muvet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -13,5 +15,13 @@ public class UserType extends AppCompatActivity {
 
         setContentView(R.layout.register_user_type);
         Button userButton = (Button) findViewById(R.id.btnRegisterUserTypeUser);
+
+        userButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(UserType.this,SignInRegister.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
