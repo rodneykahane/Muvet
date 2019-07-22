421271d9126656ab2f46873cdb2e8c17b70a2315
rodney <rodney@rodneykahan.es>
2019-07-22 14:58:33 -0400

    map added, sort of works

--- app/src/main/java/com/aaclogic/muvet/MainMap.java
+++ app/src/main/java/com/aaclogic/muvet/MainMap.java
@@ -3,7 +3,18 @@ package com.aaclogic.muvet;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainMap extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainMap extends AppCompatActivity implements OnMapReadyCallback {
+
+    private MapView mMapView;
+
+    //private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private static final String MAPVIEW_BUNDLE_KEY = "AIzaSyBlIj7M5NXPR5rMWHtxcGY7qMZtzqC5F9o";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +27,75 @@ public class MainMap extends AppCompatActivity {
         //TODO add hamburger menu
 
         //TODO add map
+
+
+
+        // *** IMPORTANT ***
+        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+        // objects or sub-Bundles.
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+        mMapView = (MapView) findViewById(R.id.mainMap);
+        mMapView.onCreate(mapViewBundle);
+
+        mMapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mMapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    @Override
+    protected void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
+
+
